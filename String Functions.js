b86ad810149e560434775ff6ab9bf48f94076247
Kiran Kumar A T <85290694+Kiran-Ambatt@users.noreply.github.com>
2021-06-30 22:20:23 +0530

    Update String Functions.js

--- String Functions.js
+++ String Functions.js
@@ -3,10 +3,8 @@ var str1 = 'Today is';
 var str2 = ' A beautiful day '
 var str3 = ' In Hawaii. '
 //Result = 'Today is a beautiful day in Hawaii.'
-var a = str2.charAt(1)
-var b = a.toLowerCase()
+var a = str2.toLowerCase()
 var c = str3.charAt(1)
 var d = c.toLowerCase()
-var e = str2.slice(2)
-var f = str3.substr(2)
-console.log(str1.concat(' ',b,e,d,f))
+var f = str3.substr(2,9)
+console.log(str1.concat(a,d,f))
